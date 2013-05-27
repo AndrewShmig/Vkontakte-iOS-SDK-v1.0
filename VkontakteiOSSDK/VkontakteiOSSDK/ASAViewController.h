e3976d290bec6e3376e5c8737b309a0631582696
AndrewShmig <andrewshmig@gmail.com>
2013-05-27 21:41:12 +0400

    Initial commit

---
+++ VkontakteiOSSDK/VkontakteiOSSDK/ASAViewController.h
@@ -0,0 +1,13 @@
+//
+//  ASAViewController.h
+//  VkontakteiOSSDK
+//
+//  Created by AndrewShmig on 5/27/13.
+//  Copyright (c) 2013 AndrewShmig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASAViewController : UIViewController
+
+@end
---
+++ VkontakteiOSSDK/VkontakteiOSSDK/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VkontakteiOSSDK
+//
+//  Created by AndrewShmig on 5/27/13.
+//  Copyright (c) 2013 AndrewShmig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ASAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ASAAppDelegate class]));
+    }
+}
