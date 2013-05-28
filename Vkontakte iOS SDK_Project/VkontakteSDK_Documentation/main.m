2b0db158338084228cf8f6fc3c29aad58b95c6c9
AndrewShmig <andrewshmig@gmail.com>
2013-05-28 14:48:37 +0400

    +new target for AppleDoc created

---
+++ Vkontakte iOS SDK_Project/VkontakteSDK_Documentation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VkontakteSDK_Documentation
+//
+//  Created by AndrewShmig on 5/28/13.
+//  Copyright (c) 2013 AndrewShmig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
